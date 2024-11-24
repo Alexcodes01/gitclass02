698662cbde1afede8282d363c3987aa745906a2d
alexcodes01 <aj.perez@live.com>
2024-11-24 18:51:28 -0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("git examples, testing commits, repos, connection to github");
+    }
+}
