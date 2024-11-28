24e57e8c900127257ff5228744753aba0fa47011
alexcodes01 <aj.perez@live.com>
2024-11-27 19:40:01 -0500

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("project version 2");
+        for (int i=0;i<100;i++){
+            System.out.println(i);
+        }
 
         System.out.println("git examples, testing commits, repos, connection to github");
     }
